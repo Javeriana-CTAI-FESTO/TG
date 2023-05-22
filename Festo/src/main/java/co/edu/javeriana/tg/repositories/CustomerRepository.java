7be075a8cc227e63920e7f294e9094ecdf75dac5
Nicolas Bayona <bayona.n@javeriana.edu.co>
2023-05-22 13:07:36 -0500

    New branch

--- Festo/src/main/java/co/edu/javeriana/tg/repositories/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.javeriana.tg.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.javeriana.tg.entities.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-}
