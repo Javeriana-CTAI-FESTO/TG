006f0dc337edee8215fb8bf59956314c88edf6c4
marioortiz6321 <marioortiz@javeriana.edu.co>
2023-05-30 21:12:22 -0500

    Cola de Producción

--- front/src/app/shared/widgets/card/card.component.ts
+++ front/src/app/shared/widgets/card/card.component.ts
@@ -28,46 +28,61 @@ export class CardComponent implements OnInit {
     {
       id: 1,
       title: 'WorkPlan',
+      state:'Completed',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 2,
       title: 'WorkPlan',
+      state:'Completed',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 3,
       title: 'WorkPlan',
+      state:'Going',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 4,
       title: 'WorkPlan',
+      state:'Incomplete',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 5,
       title: 'WorkPlan',
+      state:'Incomplete',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 6,
       title: 'WorkPlan',
+      state:'Incomplete',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 7,
       title: 'WorkPlan',
+      state:'Incomplete',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 8,
       title: 'WorkPlan',
+      state:'Incomplete',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     },
     {
       id: 9,
       title: 'WorkPlan',
+      state:'Incomplete',
+      imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
+    },
+    {
+      id: 10,
+      title: 'WorkPlan',
+      state:'Incomplete',
       imageUrl: '../../../../assets/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg'
     }
   ];
