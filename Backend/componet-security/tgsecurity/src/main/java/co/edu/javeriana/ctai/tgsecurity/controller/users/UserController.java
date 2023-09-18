f57a7df9a096466ebf42f6153042acb49c543cc6
Alejandro Sacristan Leal <49330231+AtonCode@users.noreply.github.com>
2023-09-18 10:37:27 -0500

    Eroores

--- Backend/componet-security/tgsecurity/src/main/java/co/edu/javeriana/ctai/tgsecurity/controller/users/UserController.java
+++ Backend/componet-security/tgsecurity/src/main/java/co/edu/javeriana/ctai/tgsecurity/controller/users/UserController.java
@@ -2,9 +2,12 @@ package co.edu.javeriana.ctai.tgsecurity.controller.users;
 
 import co.edu.javeriana.ctai.tgsecurity.patterns.model.Cliente;
 import co.edu.javeriana.ctai.tgsecurity.patterns.model.Order;
+import co.edu.javeriana.ctai.tgsecurity.patterns.model.User;
 import co.edu.javeriana.ctai.tgsecurity.repository.IOrderRepository;
+import co.edu.javeriana.ctai.tgsecurity.repository.IUserRepository;
 import co.edu.javeriana.ctai.tgsecurity.service.intf.IClientService;
 import co.edu.javeriana.ctai.tgsecurity.service.payload.OrderRequest;
+import co.edu.javeriana.ctai.tgsecurity.service.payload.OrderResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,6 +17,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -27,14 +31,87 @@ public class UserController {
     private IClientService clientService;
 
     @Autowired
+    private IUserRepository userRepository;
+
+    @Autowired
     private IOrderRepository orderRepository;
 
     // Queue para guardar ordenes en orden de llegada
     private Queue<Order> orderQueue = new LinkedList<>();
     RestTemplate restTemplate = new RestTemplate();
 
+    // Get Rol by user
+    @GetMapping("/rol/username={user_name}")
+    public ResponseEntity<String> getUserRole(@PathVariable String user_name) {
+
+        if (user_name != null) {
+
+            User user = userRepository.findByUsername(user_name).get();
+            System.out.println("Hello user: "+user.getUsername());
+
+            Cliente cliente = clientService.findByUsuario(user);
+
+            if (cliente != null) {
+                // El objeto cliente no es nulo, puedes acceder a sus propiedades
+
+                System.out.println("Cliente: " + cliente.getNombre());
+
+                // Determine the role
+                String role;
+                if (cliente.isAdmin()) {
+                    role = "admin";
+                } else if (cliente.isProfesor()) {
+                    role = "profesor";
+                } else if (cliente.isEstudiante()) {
+                    role = "estudiante";
+                } else {
+                    role = "cliente";
+                }
+
+                // Return a JSON response with the role
+                return ResponseEntity.ok().body("{\"rol\":\"" + role + "\"}");
+            } else {
+                // Return a 404 response if the client was not found
+                return ResponseEntity.notFound().build();
+            }
+
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("/username={user_name}");
+    }
+
+    // Get identification by user
+    @GetMapping("/cedula/username={user_name}")
+    public ResponseEntity<String> getUserCedula(@PathVariable String user_name) {
+
+        if (user_name != null) {
+
+            User user = userRepository.findByUsername(user_name).get();
+            System.out.println("Hello user: "+user.getUsername());
+
+            Cliente cliente = clientService.findByUsuario(user);
+
+            if (cliente != null) {
+                // El objeto cliente no es nulo, puedes acceder a sus propiedades
+
+                System.out.println("Cliente: " + cliente.getNombre() + " Cc: " + cliente.getIdentificacion());
+
+                // Return a JSON response with the identification
+                return ResponseEntity.ok().body("{\"cedula\":\"" + cliente.getIdentificacion() + "\"}");
+            } else {
+                // Return a 404 response if the client was not found
+                return ResponseEntity.notFound().build();
+            }
+
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("/username={user_name}");
+    }
+
     //Servicio para guardar ordenes y enviarlas al modulo FESTO
-    @PostMapping("/saveOrder")
+    @PostMapping("/save/order")
     public ResponseEntity saveOrder(@RequestBody OrderRequest orderRequest) {
 
         //TODO: Validar que el usuario exista y tenga permisos para crear ordenes
@@ -111,7 +188,7 @@ public class UserController {
     }
 
     //Servicio para consultar ordenes segun el numero de cedula del cliente
-    @GetMapping("/getOrdersByCedula/{cedula}")
+    @GetMapping("/order/cedula={cedula}")
     public ResponseEntity<Object> getOrdersByCedula(@PathVariable("cedula") String cedula) {
         try {
             // Buscar al cliente por su número de cédula
@@ -122,21 +199,33 @@ public class UserController {
                 return ResponseEntity.badRequest().build();
             }
 
-            // Luego, puedes buscar todas las órdenes del cliente
-            List<Order> ordenes = orderRepository.findByCliente(cliente);
+            // Cargar las órdenes de forma ansiosa (eager loading)
+            List<Order> ordenes = orderRepository.findByClienteFetchOrders(cliente);
+
+            // Cast to OrderResponse
+            List<OrderResponse> orderResponses = new ArrayList<>();
+            for (Order order : ordenes) {
+                orderResponses.add(new OrderResponse(
+
+                        order.getId_part(),
+                        order.getId_workPlan(),
+                        order.getCliente().getIdentificacion(),
+                        order.getTitle(),
+                        order.getOrderNumber()
+                ));
+            }
+
             System.out.println("Enviando Ordenes del cliente: " + cliente.getNombre());
 
-            return ResponseEntity.ok().body(ordenes);
+
+            return ResponseEntity.ok().body(orderResponses);
 
         } catch (NumberFormatException e) {
             // Manejo de errores si la cédula no es un número válido
             System.out.println("Cédula no válida");
             return ResponseEntity.badRequest().build();
         }
-
     }
-
-    //Servicio para validar tarjeta
 
 
 }
