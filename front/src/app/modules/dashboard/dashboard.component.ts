4cbd40094be3ca5853b44232590995add8d5cb1c
marioortiz6321 <marioortiz@javeriana.edu.co>
2023-09-18 22:40:02 -0500

    Retry after cople seconds functions

--- front/src/app/modules/dashboard/dashboard.component.ts
+++ front/src/app/modules/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard.service';
-
+import { retryWhen, delayWhen, take } from 'rxjs/operators';
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -16,12 +17,29 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.bigChart = this.dashboardService.bigChartInit();
-    this.dashboardService.getPieChartData().subscribe(data => {
+    this.dashboardService.getPieChartData().pipe(
+      retryWhen(errors => errors.pipe(
+        delayWhen(() => timer(10000)), // delay for 10 seconds before retrying
+        take(5) // retry the function 2 times in case of failure
+      ))
+    ).subscribe(data => {
       this.pieChart = data;
     });
-
+    
     this.dashboardService.ganttChart().then(data => {
       this.ganttChart = data;
+    }).catch(error => {
+      console.error('Error while fetching gantt chart data:', error);
+      // retry the function 2 times with a delay of 10 seconds in between
+      const retryGanttChart = () => {
+        this.dashboardService.ganttChart().then(data => {
+          this.ganttChart = data;
+        }).catch(error => {
+          console.error('Error while fetching gantt chart data:', error);
+          setTimeout(retryGanttChart, 10000); // retry after 10 seconds
+        });
+      };
+      setTimeout(retryGanttChart, 10000); // retry after 10 seconds
     });
   }
 
