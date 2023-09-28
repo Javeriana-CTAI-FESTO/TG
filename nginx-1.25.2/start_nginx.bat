eac8c479cfc49d0823715e0993e4ddc4f206d998
marioortiz6321 <marioortiz@javeriana.edu.co>
2023-09-28 12:59:01 -0500

    small changes

--- nginx-1.25.2/start_nginx.bat
+++ nginx-1.25.2/start_nginx.bat
@@ -5,10 +5,12 @@ set STARTUP_PATH=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
 REM Crea el archivo start_nginx.bat
 echo @echo off > %NGINX_PATH%start_nginx.bat
+echo echo Iniciando Nginx >> %NGINX_PATH%start_nginx.bat
 echo call start "" "%NGINX_PATH%nginx.exe" >> %NGINX_PATH%start_nginx.bat
+echo echo Nginx iniciado >> %NGINX_PATH%start_nginx.bat
 echo call exit >> %NGINX_PATH%start_nginx.bat
 
 REM Copia el archivo start_nginx.bat a la carpeta de inicio
+echo Copiando el archivo start_nginx.bat a la carpeta de inicio
 call copy %NGINX_PATH%start_nginx.bat %STARTUP_PATH%
-
-call exit
+echo Archivo copiado
