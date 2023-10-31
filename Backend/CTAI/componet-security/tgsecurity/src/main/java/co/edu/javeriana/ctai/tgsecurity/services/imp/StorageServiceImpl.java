45cd112982487f5b708ffd433ccc42bc413387e2
Alejandro Sacristan Leal <49330231+AtonCode@users.noreply.github.com>
2023-10-30 21:23:05 -0500

    img2

--- Backend/CTAI/componet-security/tgsecurity/src/main/java/co/edu/javeriana/ctai/tgsecurity/services/imp/StorageServiceImpl.java
+++ Backend/CTAI/componet-security/tgsecurity/src/main/java/co/edu/javeriana/ctai/tgsecurity/services/imp/StorageServiceImpl.java
@@ -9,12 +9,19 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StorageServiceImpl implements IStorageService {
+
+    private static final Logger LOGGER = Logger.getLogger(StorageServiceImpl.class.getName());
+    private static final String FILE_EMPTY_MESSAGE = "File is empty. Please select a valid file.";
+    private static final String UPLOAD_SUCCESS_MESSAGE = "File uploaded successfully: ";
+    private static final String ERROR_UPLOAD_MESSAGE = "Error uploading the file: ";
 
     private final IStorageRepository repository;
 
@@ -26,7 +33,7 @@ public class StorageServiceImpl implements IStorageService {
     public String uploadImage(MultipartFile file) {
         try {
             if (file.isEmpty()) {
-                return "File is empty. Please select a valid file.";
+                return FILE_EMPTY_MESSAGE;
             }
 
             String originalFilename = file.getOriginalFilename();
@@ -34,29 +41,42 @@ public class StorageServiceImpl implements IStorageService {
             byte[] imageData = ImageUtils.compressImage(file.getBytes());
 
             // Verificar si la imagen ya existe
-            if (repository.existsByName(originalFilename)) {
-                return "A file with the same name already exists. Please choose a different name.";
+            Optional<ImageData> existingImageData = repository.findByName(originalFilename);
+            if (existingImageData.isPresent()) {
+                // Si la imagen ya existe, simplemente sobrescribe sus datos
+                ImageData imageDataEntity = existingImageData.get();
+                imageDataEntity.setType(contentType);
+                imageDataEntity.setImageData(imageData);
+                repository.save(imageDataEntity);
+                LOGGER.log(Level.INFO, "Imagen guardada: " + originalFilename.toString());
+            } else {
+                // Si la imagen no existe, crea una nueva entidad
+                ImageData imageDataEntity = new ImageData();
+                imageDataEntity.setName(originalFilename);
+                imageDataEntity.setType(contentType);
+                imageDataEntity.setImageData(imageData);
+                repository.save(imageDataEntity);
+                LOGGER.log(Level.INFO, "Imagen sobre-escrita: " + originalFilename);
             }
 
-            ImageData imageDataEntity = new ImageData();
-            imageDataEntity.setName(originalFilename);
-            imageDataEntity.setType(contentType);
-            imageDataEntity.setImageData(imageData);
+            return UPLOAD_SUCCESS_MESSAGE + originalFilename;
 
-            repository.save(imageDataEntity);
-
-            return "File uploaded successfully: " + originalFilename;
         } catch (IOException e) {
-            return "Error uploading the file: " + e.getMessage();
+            LOGGER.log(Level.WARNING,"No se puede cargar la imagen: "+ e.getMessage(), e);
+            return ERROR_UPLOAD_MESSAGE + e.getMessage();
         }
     }
 
     @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     public byte[] downloadImage(String fileName) {
-        Optional<ImageData> dbImageData = repository.findByName(fileName);
+
+        Optional<ImageData> dbImageData = repository.findByName(fileName.toString());
+
         if (dbImageData.isPresent()) {
+            LOGGER.log(Level.INFO, "Imagen descargada: " + fileName.toString());
             return ImageUtils.decompressImage(dbImageData.get().getImageData());
         } else {
+            LOGGER.log(Level.WARNING, "Imagen no encontrada: "+fileName.toString());
             return new byte[0]; // O puedes lanzar una excepción o manejarlo de otra manera
         }
     }
--- Backend/CTAI/componet-security/tgsecurity/src/main/java/co/edu/javeriana/ctai/tgsecurity/services/utils/ImageUtils.java
+++ Backend/CTAI/componet-security/tgsecurity/src/main/java/co/edu/javeriana/ctai/tgsecurity/services/utils/ImageUtils.java
@@ -2,47 +2,62 @@ package co.edu.javeriana.ctai.tgsecurity.services.utils;
 
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
 public class ImageUtils {
 
-
     public static byte[] compressImage(byte[] data) {
         Deflater deflater = new Deflater();
-        deflater.setLevel(Deflater.BEST_COMPRESSION);
         deflater.setInput(data);
         deflater.finish();
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] tmp = new byte[4*1024];
+        byte[] buffer = new byte[1024];
+
         while (!deflater.finished()) {
-            int size = deflater.deflate(tmp);
-            outputStream.write(tmp, 0, size);
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
         }
+
+        deflater.end();
+
         try {
             outputStream.close();
-        } catch (Exception ignored) {
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
         return outputStream.toByteArray();
     }
-
-
 
     public static byte[] decompressImage(byte[] data) {
         Inflater inflater = new Inflater();
         inflater.setInput(data);
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] tmp = new byte[4*1024];
-        try {
-            while (!inflater.finished()) {
-                int count = inflater.inflate(tmp);
-                outputStream.write(tmp, 0, count);
+        byte[] buffer = new byte[1024];
+
+        while (!inflater.finished()) {
+            try {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            } catch (DataFormatException e) {
+                e.printStackTrace();
             }
-            outputStream.close();
-        } catch (Exception ignored) {
         }
+
+        inflater.end();
+
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return outputStream.toByteArray();
     }
-
 }
+
